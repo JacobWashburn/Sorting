a2e0a7280ae8bf75e1f22f8641db31d3340e63b8
Jacob Washburn <jacobrwashburn@gmail.com>
2020-04-13 14:32:11 -0600

    selection_sort done and tests passing. pep8 reformat

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,20 +1,18 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
+
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
+        # TO-DO: find next
+        # (hint, can do in 3 loc)
+        smallest_element = [cur_index, arr[i]]
+        for v in range(cur_index, len(arr)):
+            stuff = ['index', v, 'value', arr[v]]
+            smallest_element = [v, arr[v]] if smallest_element[1] > arr[v] else smallest_element
         # TO-DO: swap
-
-
-
-
+        arr[smallest_element[0]] = arr[cur_index]
+        arr[cur_index] = smallest_element[1]
     return arr
 
 
@@ -25,6 +23,9 @@ def bubble_sort( arr ):
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
-
+def count_sort(arr, maximum = -1):
     return arr
+
+
+test_arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print(selection_sort(test_arr))
