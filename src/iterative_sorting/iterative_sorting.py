250441695056e9b242e4beb7360237215a10fb93
Jacob Washburn <jacobrwashburn@gmail.com>
2020-04-13 14:43:09 -0600

    added totals to bubble and selection to track how many iterations it took to sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,18 +1,21 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-
+    total = 0
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
+
         cur_index = i
         # TO-DO: find next
         # (hint, can do in 3 loc)
         smallest_element = [cur_index, arr[i]]
         for v in range(cur_index, len(arr)):
+            total += 1
             stuff = ['index', v, 'value', arr[v]]
             smallest_element = [v, arr[v]] if smallest_element[1] > arr[v] else smallest_element
         # TO-DO: swap
         arr[smallest_element[0]] = arr[cur_index]
         arr[cur_index] = smallest_element[1]
+    print('selection total: ', total)
     return arr
 
 
@@ -30,6 +33,7 @@ def bubble_sort(arr):
                 arr[i] = next_index
                 arr[i + 1] = cur_index
                 count += 1
+    print('bubble total: ', total)
     return arr
 
 
@@ -39,5 +43,6 @@ def count_sort(arr, maximum = -1):
 
 
 test_arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+test_arr2 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 print(selection_sort(test_arr))
-print(bubble_sort(test_arr))
+print(bubble_sort(test_arr2))
